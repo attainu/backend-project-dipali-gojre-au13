5aa75f9d0ec940dc3c021f7c7c77f01bd0d881c7
dipali-gojre-au13 <dipalisag@gmail.com>
2021-03-03 18:15:06 +0530

    Created template engine

--- StoryBook/app.js
+++ StoryBook/app.js
@@ -1,15 +1,56 @@
 const express = require('express');
-const dotenv = require('dotenv')//loads environment variable .env to process.env
+const path = require('path')
 const app = express();
 const connectDB = require('./config/db')
+// const http = require('http');
+const morgan = require('morgan')
+const finalhandler = require('finalhandler')
+const exphbs = require('express-handlebars')
+const hostname = 'localhost';
+const port = 3000;
 
-//Load Config file
-dotenv.config({path:'./config/config.env'});
+
+
+// if(process.env.NODE_ENV === 'development'){
+//     app.use(morgon('dev'))
+// }
+
+
+
+//handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main',extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
+app.use('/', require('./routes/index'))
+
+//call database
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+// create "middleware"
+var logger = morgan('combined')
 
-app.listen(PORT, ()=>
-    console.log(`Server is running ${process.env.NODE_ENV} on this port is ${PORT}`)
-)
+
+
+// const server = http.createServer((req, res) => {
+ 
+//     // respond to request
+//     res.setHeader('content-type', 'text/plain')
+//     res.write('Welcome !!\n')
+//     res.end('End')
+//     })
+
+app.listen(port, () => {
+console.log(`server listening on host: ${hostname} port: ${port}`);
+});
+
+
+
+
+
+
+    // "dev": "env-cmd -f ./config/dev.env nodemon app.js"
